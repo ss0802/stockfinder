e044b5b34846391005fef15fe3c48a4fd516a68f
Saurabh Singh <ss0802@brandios.in>
2025-02-20 08:09:26 +0530

    Set up project apps

--- stockFinder/stockFinder/settings.py
+++ stockFinder/stockFinder/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
+    "users",
+    "portfolios",
+    "stocks",
+    "alerts",
+    "notifications",
+    "technical_analysis",
+
 ]
 
 MIDDLEWARE = [
