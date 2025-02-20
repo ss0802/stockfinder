29c62aab4e8ca4a4dd89b9199fe2f124b1da5136
Saurabh Singh <ss0802@brandios.in>
2025-02-20 08:35:24 +0530

    Add users app with registration and login

--- stockFinder/users/views.py
+++ stockFinder/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
